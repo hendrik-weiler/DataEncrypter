3eebd7a3365cf6eb2f205250528c55522d3ff44e
Hendrik Weiler <hendrik@ngtvbs.com>
2019-06-22 11:00:05 +0200

    Adjusted start.py for newest nwjs version

--- start.py
+++ start.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python
 import datetime
-import zipfile
 import os
 
 name = "package"
 
-def main():
-    zipper(name, name + '.nw')
-
-
-def zipper(dir, zip_file):
-    zip = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED)
-    root_len = len(os.path.abspath(dir))
-    for root, dirs, files in os.walk(dir):
-        archive_root = os.path.abspath(root)[root_len:]
-        for f in files:
-            fullpath = os.path.join(root, f)
-            archive_name = os.path.join(archive_root, f)
-            print f
-            zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
-    zip.close()
-    return zip_file
-
 if __name__ == '__main__':
-    main()
-    os.system('open -a node-webkit ./'+name+'.nw');
+    path = os.path.dirname(os.path.realpath(__file__))
+    cmd = '/Applications/nwjs/nwjs.app/Contents/MacOS/nwjs "'+path+'/' + name+'"'
+    os.system(cmd);
